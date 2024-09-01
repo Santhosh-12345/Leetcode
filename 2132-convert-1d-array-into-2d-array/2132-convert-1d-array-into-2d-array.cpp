04606f9f8e9ca9a867743da67ee9cf347a344d3f
Santhosh S <115558976+Santhosh-12345@users.noreply.github.com>
2024-09-01 23:40:41 +0530

    Time: 86 ms (42.25%), Space: 88 MB (73.42%) - LeetHub

--- 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.cpp
+++ 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.cpp
@@ -4,10 +4,9 @@ public:
         if (original.size() != m*n) 
             return {};
         vector<vector<int>> ans(m, vector<int>(n,0));
-        int k = 0;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                ans[i][j] = original[k++];
+                ans[i][j] = original[i*n+j];
             }
         }
         return ans;
