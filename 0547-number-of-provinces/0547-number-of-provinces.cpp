55bebcaec42542358eb9c0054074c31881c1be53
Santhosh S <115558976+Santhosh-12345@users.noreply.github.com>
2024-09-10 11:11:01 -0700

    Time: 18 ms (80.66%), Space: 18.2 MB (98.46%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,37 +1,49 @@
 class Solution {
 public:
-    void dfs(int Node,vector<vector<int>>&adjList,vector<int>&vis)
+vector<int>parent;
+vector<int> rank;    // To store the rank (height) of the tree for each node
+    int group; 
+    int find(int i)
     {
-        vis[Node]=1;
-        for(auto it:adjList[Node])
-        {
-            if(!vis[it])
-            dfs(it,adjList,vis);
-        }
+        while (i != parent[i]) {
+        i = parent[i];
     }
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        int V=isConnected.size(),count=0;
-        vector<int>vis(V,0);
-        vector<vector<int>>adjList(V);
-        for(int i=0;i<isConnected.size();i++)
+    return i;
+    }
+    void union1(int i,int j)
+    {
+        int pi=find(i);
+        int pj=find(j);
+        if(pi==pj)
+        return;
+        if(rank[pi]>rank[pj])
         {
-            for(int j=0;j<isConnected[0].size();j++)
-            {
-                if(isConnected[i][j]==1 && i!=j)
-                {
-                    adjList[i].push_back(j);
-                    adjList[j].push_back(i);
+            parent[pj]=pi;
+        }
+        else if(rank[pj]>rank[pi])
+        parent[pi]=pj;
+        else
+        {
+            parent[pj]=pi;
+            rank[pi]++;
+        }
+        group--;
+    }
+
+    int findCircleNum(vector<vector<int>>& isConnected) {
+       int v=isConnected.size();
+    parent.resize(v);
+       rank.resize(v,0);
+       group=v;
+       for(int i=0;i<v;i++)
+       parent[i]=i;
+       for (int i = 0; i < v; i++) {
+            for (int j = 0; j < v; j++) {
+                if (isConnected[i][j] == 1 && i != j) { 
+                    union1(i, j);
                 }
             }
         }
-        for(int i=0;i<V;i++)
-        {
-            if(!vis[i])
-            {
-                count++;
-                dfs(i,adjList,vis);
-            }
-        }
-        return count;
+       return group;
     }
 };
