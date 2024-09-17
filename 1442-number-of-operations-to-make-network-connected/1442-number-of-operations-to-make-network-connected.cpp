5e228573c0db45f0bbbed2adc60dc41768440645
Santhosh S <115558976+Santhosh-12345@users.noreply.github.com>
2024-09-17 00:10:18 -0700

    Time: 98 ms (25.41%), Space: 46.2 MB (52.18%) - LeetHub

---
+++ 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,60 @@
+class disjoint{
+    vector<int>parent;
+    vector<int>rank;
+    public:
+    disjoint(int n)
+    {
+        parent.resize(n);
+        rank.resize(n,0);
+        for(int i=0;i<n;i++)
+        parent[i]=i;
+    }
+    int findParent(int x)
+    {
+        if(x==parent[x])
+        return x;
+        return parent[x]=findParent(parent[x]);
+    }
+    void unionSets(int u,int v)
+    {
+        int pu=findParent(u);
+        int pv=findParent(v);
+        if(pu==pv)
+        return;
+        else if(rank[pu]>rank[pv])
+        parent[pv]=pu;
+        else if(rank[pv]>rank[pu])
+        parent[pu]=pv;
+        else
+        {
+            parent[pv]=pu;
+            rank[pu]++;
+        }
+    }
+};
+class Solution {
+public:
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        int extra=0,ans=0;
+        disjoint ds(n);
+        for(auto it:connections)
+        {
+            int u=it[0];
+            int v=it[1];
+            if(ds.findParent(u)==ds.findParent(v))
+            extra++;
+            else
+            ds.unionSets(u,v);
+        }
+        int count=0;
+        for(int i=0;i<n;i++)
+        {
+            if(ds.findParent(i)==i)
+            ans++;
+        }
+        int required=ans-1;;
+        if(extra>=required)
+        return required;
+        return -1;
+    }
+};
