863c63530a75ece4252cf0ab938b7b4af1d95b86
Santhosh S <115558976+Santhosh-12345@users.noreply.github.com>
2025-02-10 08:10:04 +0530

    Time: 2 ms (20.65%), Space: 9 MB (49.32%) - LeetHub

---
+++ 3447-clear-digits/3447-clear-digits.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        stack<char>st;
+        for(char c:s)
+        {
+            if(isdigit(c))
+            {
+                if(!st.empty() && isalpha(st.top()))
+                st.pop();
+            }
+            else
+            st.push(c);
+        }
+        string ans="";
+        while(!st.empty())
+        {
+            ans+=st.top();
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
