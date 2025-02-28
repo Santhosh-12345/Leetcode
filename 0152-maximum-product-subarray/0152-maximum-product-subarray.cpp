228642a53f7c02bced0630464da34bbf4ee623a9
Santhosh S <115558976+Santhosh-12345@users.noreply.github.com>
2025-02-28 20:30:50 +0530

    Time: 0 ms (100%), Space: 17.7 MB (84.18%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,14 +1,20 @@
 class Solution {
 public:
-    int maxProduct(vector<int>& nums) {
-        int result=nums[0],prod1=nums[0],prod2=nums[0];
-        for(int i=1;i<nums.size();i++)
-        {
-            int temp=max(nums[i],max(nums[i]*prod1,nums[i]*prod2));
-            prod2=min(nums[i],min(nums[i]*prod1,nums[i]*prod2));
-            prod1=temp;
-            result=max(result,prod1);
-        }
-        return result;
+    int maxProduct(vector<int>& arr) {
+        long long maxProd = arr[0]; 
+    long long minProd = arr[0]; 
+    long long result = arr[0];  
+    int n=arr.size();
+    for (int i = 1; i < n; i++) {
+        if (arr[i] < 0) 
+            swap(maxProd, minProd); // Swap because multiplying by negative flips min/max
+
+        maxProd = max((long long)arr[i], maxProd * arr[i]); // Max product ending at i
+        minProd = min((long long)arr[i], minProd * arr[i]); // Min product ending at i
+
+        result = max(result, maxProd); // Update overall max product
+    }
+
+    return result;
     }
 };
