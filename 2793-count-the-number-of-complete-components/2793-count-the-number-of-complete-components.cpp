39e6be4983dbcf9be43acf26c5b8444408100bac
Santhosh S <115558976+Santhosh-12345@users.noreply.github.com>
2025-03-22 12:30:15 +0530

    Time: 25 ms (88.59%), Space: 128 MB (80.25%) - LeetHub

--- 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.cpp
+++ 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.cpp
@@ -1,36 +1,39 @@
 class Solution {
 public:
-    void dfs(int i,vector<bool>&vis,int& e,int& node,vector<vector<int>>&adj)
-    {
-        node++;
-        vis[i]=true;
-        for(auto it:adj[i])
-        {
-            e++;
-            if(!vis[it])
-            {
-                dfs(it,vis,e,node,adj);
+    void dfs(int i, vector<bool>& vis, int &e, int &n, vector<vector<int>>& adj) {
+        n++; 
+        vis[i] = true;  
+        for(auto it : adj[i]) {
+            e++;  
+            if(!vis[it]) {
+                dfs(it, vis, e, n, adj);
             }
         }
     }
+
     int countCompleteComponents(int n, vector<vector<int>>& edges) {
-        int ans=0;
-        vector<bool>vis(n,false);
-        vector<vector<int>>adj(n);
-        for(auto it:edges)
-        {
+        int ans = 0;
+        vector<bool> vis(n, false); 
+        vector<vector<int>> adj(n);
+
+        // Build adjacency list
+        for(auto& it : edges) {
             adj[it[0]].push_back(it[1]);
             adj[it[1]].push_back(it[0]);
         }
-        for(int i=0;i<n;i++)
-        {
-            if(!vis[i]){
-                int node=0,e=0;
-                dfs(i,vis,e,node,adj);
-                if(e==node*(node-1))
-                ans+=1;
+
+        // Traverse all nodes
+        for(int i = 0; i < n; i++) {
+            if(!vis[i]) {
+                int compNodes = 0, compEdges = 0;
+                dfs(i, vis, compEdges, compNodes, adj);
+                
+                // Correct edge count: each edge is counted twice in an undirected graph
+                if (compEdges / 2 == compNodes * (compNodes - 1) / 2)
+                    ans++;
             }
         }
+
         return ans;
     }
 };
