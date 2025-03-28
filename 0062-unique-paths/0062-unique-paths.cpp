7652a15837ae980bc1487a75798d815fe756bc0e
Santhosh S <115558976+Santhosh-12345@users.noreply.github.com>
2025-03-28 18:14:41 +0530

    Time: 0 ms (100%), Space: 9.3 MB (32.71%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int count(int m,int n,vector<vector<int>>& dp,int i,int j)
+    {
+        if(i==m-1 && j==n-1)
+        return 1;
+        if(i>=m || j>=n)
+        return 0;
+        if(dp[i][j]!=-1)
+        return dp[i][j];
+        return dp[i][j]=count(m,n,dp,i+1,j)+count(m,n,dp,i,j+1);
+    }
+    int uniquePaths(int m, int n) {
+        vector<vector<int>>dp(m+1,vector<int>(n,-1));
+        return count(m,n,dp,0,0);
+    }
+};
