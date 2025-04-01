310fe2a558a25a8494c3202374dff0c2a6404a45
Santhosh S <santhoshsiva2909@gmail.com>
2025-04-01 07:49:18 +0530

    Time: 11 ms (32.59%), Space: 127.5 MB (36.85%) - LeetHub

---
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    long long func(int ind,vector<vector<int>>& questions,vector<long long>& dp)
+    {
+        if(ind>=questions.size())
+        {
+            return 0;
+        }
+        if(dp[ind]!=-1)
+        return dp[ind];
+        long long pick=questions[ind][0]+func(ind+questions[ind][1]+1,questions,dp);
+        long long notpick=func(ind+1,questions,dp);
+        return dp[ind]=max(pick,notpick);
+    }
+    long long mostPoints(vector<vector<int>>& questions) {
+        vector<long long>dp(questions.size(),-1);
+        return func(0,questions,dp);
+    }
+};
