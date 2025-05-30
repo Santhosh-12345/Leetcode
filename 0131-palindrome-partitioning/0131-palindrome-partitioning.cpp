a6593d364e37149be08bd470087afbc62f470e2a
Santhosh S <santhoshsiva2909@gmail.com>
2025-05-30 08:17:36 +0530

    Time: 66 ms (27.97%), Space: 61.7 MB (44.04%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    void func(int index,vector<vector<string>>&ans,vector<string>&path,string s)
+    void func(int ind,string s,vector<string>&path,vector<vector<string>>&ans)
     {
-        if(index==s.size())
+        if(ind==s.size())
         {
             ans.push_back(path);
             return;
         }
-        for(int i=index;i<s.size();i++)
+        for(int i=ind;i<s.size();i++)
         {
-            if(isPalindrome(index,i,s))
+            if(isValid(s,ind,i))
             {
-                path.push_back(s.substr(index,i-index+1));
-                func(i+1,ans,path,s);
+                path.push_back(s.substr(ind,i-ind+1));
+                func(i+1,s,path,ans);
                 path.pop_back();
             }
         }
     }
-    bool isPalindrome(int start,int end,string s)
+    bool isValid(string s,int start,int end)
     {
-        while(start<end)
+        while(start<=end)
         {
             if(s[start]!=s[end])
             return false;
@@ -29,9 +29,9 @@ public:
         return true;
     }
     vector<vector<string>> partition(string s) {
-        vector<vector<string>> ans;
-        vector<string> path;
-        func(0,ans,path,s);
+        vector<vector<string>>ans;
+        vector<string>path;
+        func(0,s,path,ans);
         return ans;
     }
 };
