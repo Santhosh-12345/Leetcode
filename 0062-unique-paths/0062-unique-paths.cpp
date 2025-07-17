7cee11c06793e7b1f67b1caa62deafbfb5ff6364
Santhosh S <santhoshsiva2909@gmail.com>
2025-07-17 08:28:33 +0530

    Time: 0 ms (100%), Space: 7.9 MB (86.58%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
-    int count(int m,int n,vector<vector<int>>& dp,int i,int j)
-    {
-        if(i==m-1 && j==n-1)
-        return 1;
-        if(i>=m || j>=n)
-        return 0;
-        if(dp[i][j]!=-1)
-        return dp[i][j];
-        return dp[i][j]=count(m,n,dp,i+1,j)+count(m,n,dp,i,j+1);
-    }
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m+1,vector<int>(n,-1));
-        return count(m,n,dp,0,0);
+        int N = m + n - 2;     // Total moves
+        int r = min(m - 1, n - 1);   // Choose smaller of down or right moves
+
+        long long result = 1;   // To prevent overflow
+
+        for(int i = 1; i <= r; i++) {
+            result = result * (N - i + 1);
+            result = result / i;
+        }
+
+        return (int)result;
     }
 };
