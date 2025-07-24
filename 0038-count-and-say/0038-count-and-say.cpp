c7e97e9f97cb2c4784fd60eaca8f3f29207799a5
Santhosh S <santhoshsiva2909@gmail.com>
2025-07-24 18:25:50 +0530

    Time: 3 ms (83.18%), Space: 10.2 MB (36.51%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -3,21 +3,23 @@ public:
     string countAndSay(int n) {
         if(n==1)
         return "1";
-        string result="";
-        int count=1;
         string prev=countAndSay(n-1);
+        string ans="";
+        int count=1;
         for(int i=1;i<prev.size();i++)
         {
             if(prev[i]==prev[i-1])
-            count++;
-            else{
-                result+=to_string(count)+prev[i-1];
+            {
+                count++;
+            }
+            else
+            {
+                ans+=to_string(count)+prev[i-1];
                 count=1;
             }
         }
-        result+=to_string(count)+prev.back();
-        
-        return result;
+        ans+=to_string(count)+prev.back();
+        return ans;
     }
     
 };
